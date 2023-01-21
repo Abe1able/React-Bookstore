8ba147aa06fd38f9773ec0c2c5870b39d4826b17
Abel Seifu <abel.workmail@gmail.com>
2023-01-21 16:11:51 +0300

    Add check_status button according to review

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Categories = () => (
   <div>
-    <h2>Under construction</h2>
+    <button type="button">Check Status</button>
   </div>
 );
 export default Categories;
