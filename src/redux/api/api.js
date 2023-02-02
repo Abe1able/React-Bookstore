da1829e423a738bd9bd8b4d8e3d206912f0e9af0
Abel <abel.workmail@gmail.com>
2023-02-02 22:26:06 +0300

    Create functions to add and remove books

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,8 +1,25 @@
-const URL =
-  "https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/MUUGSyLKPFVh40o1XTap/books";
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/MUUGSyLKPFVh40o1XTap/books';
 
-const getBooks = async () => {
+export const getBooks = async () => {
   const getBookItems = await fetch(URL).then((res) => res.json());
   console.log(getBookItems);
   return getBookItems;
+};
+
+export const addBooksItem = async (elements) => {
+  const postBooks = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(elements),
+  }).then((response) => response.text());
+  return postBooks;
+};
+
+export const removeBookItem = async (id) => {
+  const removeBook = await fetch(`URL${id}`, {
+    method: 'DELETE',
+    }).then((response) => response.text());
+    return removeBook;
 };
