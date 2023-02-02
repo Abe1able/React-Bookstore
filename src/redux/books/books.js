5fb8ce0c02e6f246a3d1690f848e7cb3630f58fe
Abel <abel.workmail@gmail.com>
2023-02-02 22:45:34 +0300

    Refactor addBook and removeBook action creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getBooks, removeBookItem, addBooksItem } from '../api/api';
 
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookstore/books/GET_BOOK';
 
 const initialState = [
   {
@@ -18,24 +21,27 @@ const initialState = [
   },
 ];
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }
 };
+
+export const addBook = createAsyncThunk(ADD_BOOK, 
+  async (elem) => {
+    await addBooksItem(elem);
+    return elem;
+  }, 
+  );
+
+export const removeBook = createAsyncThunk(REMOVE_BOOK, async (id) => {
+  await removeBooksItem(id);
+  return elem;
+});
 
 export default reducer;
